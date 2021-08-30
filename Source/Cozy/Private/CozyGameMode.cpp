47192fb281ab5fa81db71ee140ae86b81dd442f1
Hunter Ovadia <hunter.ovadia@gmail.com>
2021-08-29 19:06:15 -0700

    Initial Project Setup

--- Source/Cozy/Private/CozyGameMode.cpp
+++ Source/Cozy/Private/CozyGameMode.cpp
@@ -1,15 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "CozyGameMode.h"
-#include "CozyCharacter.h"
-#include "UObject/ConstructorHelpers.h"
-
 ACozyGameMode::ACozyGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
 }
--- Source/Cozy/Public/CozyGameMode.h
+++ Source/Cozy/Public/CozyGameMode.h
@@ -1,7 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "CozyGameMode.generated.h"
